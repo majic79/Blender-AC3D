3a6a0be3b51b7428e68bc3d6a41e969643de1d39
Nikolai V. Chr <emptyyetfull@gmail.com>
2016-05-15 08:45:00 +0200

    Version 2.9

--- io_scene_ac3d/__init__.py
+++ io_scene_ac3d/__init__.py
@@ -34,7 +34,7 @@ bl_info = {
 	"name": "AC3D (.ac) format",
 	"description": "AC3D model exporter for blender.",
 	"author": "Willian P Gerano, Chris Marr, Thomas Geymayer, Nikolai V. Chr.",
-	"version": (2,8),
+	"version": (2,9),
 	"blender" : (2,6,0),
 	"api": 41098,
 	"location": "File > Import-Export",
